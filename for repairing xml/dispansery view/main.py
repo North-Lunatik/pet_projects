5b18f963c2841c5e76f88042af3566652acbbe62
Striganov <vitek161147@yandex.ru>
2023-02-05 14:05:15 +0300

    Дополняет форму справкой по использованию и выводом комментариев к работе

--- for repairing xml/dispansery view/main.py
+++ for repairing xml/dispansery view/main.py
@@ -2,14 +2,23 @@ import os
 from datetime import datetime
 from itertools import cycle
 from tkinter import BOTH, END, Tk, filedialog, messagebox
+from tkinter.scrolledtext import ScrolledText
 from tkinter.ttk import Button, Entry, Frame, Label
-from typing import Tuple
+from typing import List, Tuple, Union
 
 import xlrd
 from lxml import etree
 from lxml.etree import Element, tostring
 
+help = """
+Программа предназначена для исправления данных в некорректно сформированном xml по ДН.
 
+Информацию предполагается брать из отчета: `Список пациентов, запланированных для диспансерного наблюдения`
+
+Файл отчета xls формируется несколько некорректно. 
+Для исправления можно выполнить "Сохранить как" в формате excel 98/2003 и после этого выбирать для обработки.
+    
+"""
 def get_data_from_report(filename) -> Tuple[dict]:
     """Возвращает записи отчета, в которых указана дата последней явки и вспомогательные данные."""
     report_data = {}
@@ -42,7 +51,7 @@ def get_data_from_report(filename) -> Tuple[dict]:
     return report_data, phone_data
 
 def check_duplicates(file_path: str) -> Tuple[str, int]:
-    """Возвращает сводку по наличию дубликатов в итоговой xml по параметрам fio, dr, ds"""
+    """Возвращает сводку по наличию дубликатов в итоговой xml по параметрам fio, dr, ds."""
     result = {}
 
     tree = etree.parse(file_path)
@@ -74,7 +83,6 @@ def remove_duplicates(file_path: str) -> None:
 
     with open(file_path, "w", encoding='cp1251', errors=None, newline='\r\n') as f:
         f.write(tostring(root, pretty_print=True, encoding='Windows-1251', xml_declaration=True).decode('cp1251'))
-    print('Дубликаты удалены.')
 
 def get_ot_data(ot_obj: Element) -> str:
     """Возвращает обработанное отчество."""
@@ -93,11 +101,12 @@ class Application(Frame):
         super().__init__()
         self.xml_filepath = None
         self.report_filepath = None
+        self.console = None
         self.initUI()
 
-    def initUI(self):
+    def initUI(self) -> None:
         """Инициализирует UI."""
-        self.master.title('Костыль для подстановки диагнозов')
+        self.master.title('Костыль для подстановки недостающих данных.')
         self.pack(fill=BOTH, expand=True)
 
         # Путь до отчета
@@ -118,29 +127,56 @@ class Application(Frame):
 
         run_button = Button(self, text="Преобразовать", command=self.rebuild_xml)
         run_button.grid(row=3, column=2)
+        help_button = Button(self, text="Справка", command=self.show_help)
+        help_button.grid(row=3, column=3)
 
-    def open_report_file(self):
+        # Виджет для отображаения результатов обработки
+        self.console = ScrolledText(self, height=10)
+        self.console.grid(row=4, column=1, columnspan=3)
+
+    def to_console(self, msgs: Union[str, List[str]]) -> None:
+        """Добавляет строку в виджет вывода результатов на экран."""
+        if self.console:
+            if isinstance(msgs, str):
+                msgs = [msgs]
+            
+            self.console.update_idletasks()
+            for msg in msgs:
+                self.console.insert("end", msg + '\n')
+
+    def open_report_file(self) -> None:
         """Устанавливает путь к отчету."""
         self.report_filepath = filedialog.askopenfilename()
         self.report_field.delete("0", END)
         self.report_field.insert(0, self.report_filepath)
 
-    def open_xml_file(self):
+    def open_xml_file(self) -> None:
         """Устанавливает путь к xml."""
         self.xml_filepath = filedialog.askopenfilename()
         self.xml_field.delete("0", END)
         self.xml_field.insert(0, self.xml_filepath)
 
-    def rebuild_xml(self):
-        """Обрабатываем файл."""
+    def show_help(self) -> None:
+        """Выводит диалоговое окно со справкой по использованию."""
+
+        messagebox.showinfo('Справка по использованию.', help)
+
+    def rebuild_xml(self) -> None:
+        """Обрабатывает файл."""
         if not self.report_filepath:
-            messagebox.showerror("Ошибка", "Не выбран файл с отчетом.")
+            messagebox.showerror("Ошибка", "Не выбран файл отчета.")
+            return
         if not self.xml_filepath:
             messagebox.showerror("Ошибка", "Не выбран xml файл.")
+            return
+        
         report_data = None
         phone_data = None
         xml_file_path = self.xml_filepath
+
+        self.to_console('Обработка файла отчета...')
         report_data, phone_data = get_data_from_report(str(self.report_filepath))
+        self.to_console('Завершено.')
 
         # фиксируем позицию элементов
         prepared_report_data = {}
@@ -149,6 +185,7 @@ class Application(Frame):
             data.sort()
             prepared_report_data[x] = cycle(data)
 
+        self.to_console('Обработка xml файла...')
         tree = etree.parse(xml_file_path)
         root = tree.getroot()
         for zap in root.findall('ZAP'):
@@ -171,22 +208,23 @@ class Application(Frame):
                         if phones:
                             zap.find('PHONE').text = phones[0]
                     else:
-                        print(f'Для {fio} {dr_str} не найдено данных в отчете')
+                        self.to_console(f'Для {fio} {dr_str} не найдено данных в отчете')
 
         result_path = os.path.join(os.getcwd(), 'result.xml')
         with open(result_path, "w", encoding='cp1251', errors=None, newline='\r\n') as f:
             f.write(tostring(root, pretty_print=True, encoding='Windows-1251', xml_declaration=True).decode('cp1251'))
-
+        
+        self.to_console(['Завершено.', '', 'Поиск дубликатов в xml...'])
         result_text, count = check_duplicates(result_path)
-        print(result_text)
         if count > 0:
+            self.to_console(result_text)
             remove_duplicates(result_path)
+            self.to_console('Дубликаты удалены.')
 
-        print('Готово.')
+        self.to_console(['', 'Готово.'])
 
 
 def main():
-
     root = Tk()
     app = Application()
     root.mainloop()
